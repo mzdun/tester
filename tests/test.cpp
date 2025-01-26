44ff605a0923717545dda55115ab64037fd5c243
Marcin Zdun <marcin.zdun@gmail.com>
2025-01-26 14:55:51 +0100

    Initial commit

---
+++ tests/test.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Marcin Zdun
+// This code is licensed under MIT license (see LICENSE for details)
+
+#include <fmt/format.h>
+#include <gtest/gtest.h>
+
+namespace wall::testing {
+
+	TEST(wall, test) {
+		ASSERT_EQ(1, 1);
+	}
+
+}  // namespace wall::testing
