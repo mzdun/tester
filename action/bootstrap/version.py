9f675f1af753a153b65b62a8b98da3536bde78b3
Marcin Zdun <marcin.zdun@gmail.com>
2025-02-24 21:39:39 +0100

    work with action/bootstrap

---
+++ action/bootstrap/version.py
@@ -0,0 +1,31 @@
+import os
+
+VAR_NAME = "CXX_FLOW_VERSION"
+VARIABLE = ""
+
+try:
+    with open(".flow/flow.py", encoding="UTF-8") as flow_py:
+        for line in flow_py:
+            if not line.startswith(VAR_NAME):
+                continue
+
+            var_list = line.split("=", 1)
+            if len(var_list) != 2:
+                continue
+
+            var = var_list[0].strip()
+            if var != VAR_NAME:
+                continue
+
+            val = var_list[1].strip()
+            start = val[:1]
+            if start not in "\"'":
+                continue
+
+            VARIABLE = val[1:].split(start, 1)[0].strip()
+            break
+except FileNotFoundError:
+    pass
+
+with open(os.environ["GITHUB_OUTPUT"], "a", encoding="UTF-8") as out:
+    print(f"value={VARIABLE}", file=out)
